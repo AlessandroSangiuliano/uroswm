358fc2efeae656125494e32ecb1789a4e7edc9fc
Alessandro Sangiuliano <alex22_7@hotmail.com>
2020-06-22 10:58:51 +0200

    Initial Commit

---
+++ uroswm/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  uroswm
+//
+//  Created by Alessandro Sangiuliano on 22/06/20.
+//  Copyright (c) 2020 Alessandro Sangiuliano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

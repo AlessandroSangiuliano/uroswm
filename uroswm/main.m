593f86c9859ba388102d61bbcd443e0dbf400f7a
Alex <alex22_7@hotmail.com>
2021-02-10 07:16:52 +0100

    Set positions for title bar buttons

--- uroswm/main.m
+++ uroswm/main.m
@@ -19,7 +19,13 @@ int main(int argc, const char * argv[])
         NSLog(@"Starting uroswm...");
 
         TitleBarSettingsService *settings = [TitleBarSettingsService sharedInstance];
-        [settings setHeight:23];
+        [settings setHeight:25];
+        XCBPoint closePosition = XCBMakePoint(3.5,3.8);
+        XCBPoint minimizePosition = XCBMakePoint(3,8);
+        XCBPoint maximizePosition = XCBMakePoint(3,3);
+        [settings setClosePosition:closePosition];
+        [settings setMinimizePosition:minimizePosition];
+        [settings setMaximizePosition:maximizePosition];
 
         URSEventHandler* ursHandler = [[URSEventHandler alloc] init];
         [ursHandler registerAsWindowManager];
